c34d6bd6edbaea4107fb2a0c33a006f79975cf6d
Achal Venkatesh <thorsthorfinn2003@gmail.com>
2024-02-07 22:16:57 +0530

    Connected mysql db

---
+++ api/src/server.go
@@ -0,0 +1,84 @@
+package main
+
+import(
+  "fmt"
+  "net/http"
+  "github.com/gin-gonic/gin"
+  "os"
+  "log"
+  "database/sql"
+  "github.com/go-sql-driver/mysql"
+)
+
+type users struct{
+  Full_name string `json:"full_name"`
+  Email     string `json:"email"`
+  Password  string `json:"password"`
+  Username  string `json:"username"`
+}
+
+func connectToDb(db *sql.DB)(*sql.DB, error){
+  cfg := mysql.Config{
+        User:   os.Getenv("DBUSER"),
+        Passwd: os.Getenv("DBPASS"),
+        Net:    "tcp",
+        Addr:   "127.0.0.1:3306",
+        DBName: "travel_jinn",
+    }
+    var err error
+    db, err = sql.Open("mysql", cfg.FormatDSN())
+    if err != nil {
+        return nil, err
+    }
+
+    if err := db.Ping(); err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    fmt.Println("Connected!")
+    return db , nil;
+}
+
+func main(){
+  var db *sql.DB
+  var err error
+  db,err = connectToDb(db)
+  if err!=nil{
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+  router := gin.Default()
+  router.POST("/signin",createUser(db))
+  router.Run("localhost:3050")
+
+}
+
+func createUser(db *sql.DB) gin.HandlerFunc{
+  return func(c *gin.Context){
+    var err error
+    var user users
+    if err = c.ShouldBindJSON(&user);err!=nil{
+            c.String(http.StatusBadRequest, "Bad request")
+            return
+        }
+
+    result, err := db.Exec("INSERT INTO users (email, password, username, full_name) VALUES (?, ?, ?, ?)", user.Email, user.Password, user.Username, user.Full_name)
+    if err != nil {
+      log.Printf("Error inserting user into database: %v", err)
+      c.String(http.StatusInternalServerError, "Internal Server Error")
+      return
+    }
+
+        id, err := result.LastInsertId()
+        if err != nil {
+            log.Printf("Error retrieving last insert ID: %v", err)
+            c.String(http.StatusInternalServerError, "Internal Server Error")
+            return
+        }
+
+        c.JSON(http.StatusCreated, gin.H{"id": id})
+    }
+}
+
